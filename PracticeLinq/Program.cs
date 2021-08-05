a42be977492c6914e175aed3508d89e82970d513
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-08-05 11:35:57 -0700

    Inital Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> videoGames = new List<string>();
+            
+            //storeing list of Games
+                videoGames.Add("Candy Crush.");
+                videoGames.Add("Mario Brothers.");
+                videoGames.Add("Sonic.");
+                videoGames.Add("Dinner Dash.");
+
+           //======================================
+            //Order List im asending order by length.
+          //=======================================
+
+            var orderList = videoGames.OrderBy(x => x.Count());
+
+            //iterataring through list and loging order list to the console.
+
+            foreach (var item in orderList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
